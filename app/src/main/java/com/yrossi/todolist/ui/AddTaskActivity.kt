57899b0d85ba6a5b27479ad0294cd4ce770f16f7
YcaroRossi <ycarorossi@gmail.com>
2021-09-08 11:11:41 -0300

    Initial commit

---
+++ app/src/main/java/com/yrossi/todolist/ui/AddTaskActivity.kt
@@ -0,0 +1,86 @@
+package com.yrossi.todolist.ui
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import com.yrossi.todolist.databinding.ActivityAddTaskBinding
+import com.yrossi.todolist.datasource.TaskDataSource
+import com.yrossi.todolist.extensions.format
+import com.yrossi.todolist.extensions.text
+import com.yrossi.todolist.model.Task
+import java.util.*
+
+class AddTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddTaskBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (intent.hasExtra(TASK_ID)) {
+            val taskId = intent.getIntExtra(TASK_ID, 0)
+            TaskDataSource.findById(taskId)?.let {
+                binding.tilTitle.text = it.title
+                binding.tilDate.text = it.date
+                binding.tilTime.text = it.time
+            }
+        }
+        
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.tilDate.editText?.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker().build()
+
+            datePicker.addOnPositiveButtonClickListener {
+                val timeZone = TimeZone.getDefault()
+                val offset = timeZone.getOffset(Date().time) * -1
+                binding.tilDate.text = Date(it + offset).format()
+            }
+            datePicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+        }
+
+        binding.tilTime.editText?.setOnClickListener {
+            val timePicker = MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .build()
+
+            timePicker.addOnPositiveButtonClickListener {
+                val minute = if (timePicker.minute in 0..9) "0${timePicker.minute}" else timePicker.minute
+                val hour = if (timePicker.hour in 0..9) "0${timePicker.hour}" else timePicker.hour
+
+                binding.tilTime.text = "${hour}:${minute}"
+            }
+            timePicker.show(supportFragmentManager, null)
+        }
+
+        binding.btnCancel.setOnClickListener{
+            finish()
+        }
+
+        binding.btnCreateTask.setOnClickListener{
+            val task = Task(
+                title = binding.tilTitle.text,
+                date = binding.tilDate.text,
+                time = binding.tilTime.text,
+                id = intent.getIntExtra(TASK_ID, 0)
+            )
+            TaskDataSource.insertTask(task)
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
+    }
+
+    companion object {
+        const val TASK_ID = "task_id"
+    }
+}
